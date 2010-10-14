275e096462f4ae2978358d64e8a8a0053ac420ea
gmthomas25 <gmthomas25@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-10-14 00:48:39 +0000

    Greg_Commit_TimeNotification

--- Classes/NotificationTime.m
+++ Classes/NotificationTime.m
@@ -10,6 +10,7 @@
 
 
 @implementation NotificationTime
+@synthesize datePicker, eventText, delegateReference, morningEvening;
 
 /*
  // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
@@ -21,11 +22,139 @@
 }
 */
 
+/*-(id) init{
+	self = [super initWithNibName:@"NotificationTime" bundle:nil];
+	
+	return self;
+	
+}*/
+
+
+-(IBAction)goToHomeScreen:(id)sender{
+	[self.view removeFromSuperview];
+	
+	UILocalNotification *localNotifMorning = [[UILocalNotification alloc] init];
+	UILocalNotification *localNotifEvening = [[UILocalNotification alloc] init];
+    if (localNotifMorning == nil)
+        return;
+	if (localNotifEvening == nil)
+        return;
+	
+	localNotifEvening.fireDate = eveningDate;
+	localNotifMorning.fireDate = morningDate;
+	
+	localNotifEvening.timeZone = [NSTimeZone defaultTimeZone];
+	localNotifEvening.timeZone = [NSTimeZone defaultTimeZone];
+	
+	localNotifEvening.repeatInterval = NSDayCalendarUnit;
+	localNotifMorning.repeatInterval = NSDayCalendarUnit;
+	
+	localNotifMorning.alertBody = @"Theme of suggestion";
+	localNotifMorning.alertAction = @"See More";
+	localNotifEvening.alertBody = @"How was your day?";
+	localNotifEvening.alertAction = @"Reflect";
+	
+	[[UIApplication sharedApplication] scheduleLocalNotification:localNotifEvening];
+	[[UIApplication sharedApplication] scheduleLocalNotification:localNotifMorning];
+	
+	[localNotifEvening release];
+	[localNotifMorning release];
+	
+	/*localNotif.fireDate = [itemDate dateByAddingTimeInterval:1];
+	
+	//1 day repeat interval
+	localNotif.repeatInterval = NSDayCalendarUnit;
+	// localNotif.fireDate = [NSDate dateWithTimeIntervalSinceNow:20];
+    localNotif.timeZone = [NSTimeZone defaultTimeZone];
+	
+	// Notification details
+    localNotif.alertBody = @"You should reflect";
+	// Set the action button
+    localNotif.alertAction = @"Reflect";
+	
+    localNotif.soundName = UILocalNotificationDefaultSoundName;
+    localNotif.applicationIconBadgeNumber = 1;
+	
+	// Specify custom data for the notification
+    NSDictionary *infoDict = [NSDictionary dictionaryWithObject:@"someValue" forKey:@"someKey"];
+    localNotif.userInfo = infoDict;
+	
+	// Schedule the notification
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotif];
+    [localNotif release];*/
+	
+
+	[delegateReference.window addSubview:delegateReference.tabController.view];
+	
+}
+
+-(IBAction)dateChanged:(id)sender{
+	if(morningEvening.selectedSegmentIndex == 0){
+		morningDate = [datePicker date];
+		[morningDate retain];
+		NSLog(@"Changing morning date");
+	}
+	else{
+		NSLog(@"Changing evening date");
+		eveningDate = [datePicker date];
+		[eveningDate retain];
+	}
+	
+}
+
+-(IBAction)segmentChange:(id)sender{
+	if(morningEvening.selectedSegmentIndex == 0){
+		NSLog(@"InsideMorning Segment");
+		[datePicker setDate:morningDate];
+	}
+	else {
+		NSLog(@"Inside Evening Segment");
+		NSLog([eveningDate description]);
+		assert(eveningDate != nil);
+		[datePicker setDate:eveningDate];
+	}
+
+}
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
-	delegateReference = [[UIApplication sharedApplication] delegate];
-	[datePicker setDate:[NSDate date]];
+	//delegateReference = [[UIApplication sharedApplication] delegate];
+	
+	NSCalendar *calendar = [NSCalendar autoupdatingCurrentCalendar];
+	
+	[[UIApplication sharedApplication] cancelAllLocalNotifications];
+	
+    // Get the current date
+    NSDate *pickerDate = [datePicker date];
+	
+    // Break the date up into components
+    NSDateComponents *dateComponents = [calendar components:( NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit )
+												   fromDate:pickerDate];
+    NSDateComponents *timeComponents = [calendar components:( NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit )
+												   fromDate:pickerDate];
+    // Set up the fire time
+    NSDateComponents *dateComps = [[NSDateComponents alloc] init];
+	//NSDateComponents *dateComp1 = [[NSDateComponents alloc] init];
+    [dateComps setDay:[dateComponents day]];
+    [dateComps setMonth:[dateComponents month]];
+    [dateComps setYear:[dateComponents year]];
+    [dateComps setHour:10];
+	[dateComps setMinute:0];
+	
+	//[dateComp1 setHour:22];
+	//[dateComp1 setMinute:0];
+
+	// Notification will fire in one minute
+	//[dateComps setSecond:[timeComponents second]];
+	morningDate =[calendar dateFromComponents:dateComps];
+	[dateComps setHour:22];
+	eveningDate = [calendar dateFromComponents:dateComps];
+	[eveningDate retain];
+	[morningDate retain];
+	NSLog([eveningDate description]);
+	[datePicker setDate:morningDate];
+	[dateComps release];
+	
     [super viewDidLoad];
 	
 	//[[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
@@ -40,7 +169,7 @@
 }
 */
 
-@synthesize datePicker, eventText, delegateReference;
+
 
 - (IBAction) removeAlarm:(id) sender{
 	
