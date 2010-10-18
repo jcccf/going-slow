7a6ade9314f6d295ba4516bcfcc844f84147413b
gmthomas25 <gmthomas25@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-10-18 21:36:17 +0000

    Added_Reflection

---
+++ goslowtest2.xcdatamodeld/Reflection.m
@@ -0,0 +1,21 @@
+//
+//  Reflection.m
+//  goslowtest2
+//
+//  Created by Gregory Thomas on 10/18/10.
+//  Copyright 2010 Cornell University. All rights reserved.
+//
+
+#import "Reflection.h"
+
+
+@implementation Reflection
+
+@dynamic reflectionPicturePath;
+@dynamic colorDayRed;
+@dynamic colorDayGreen;
+@dynamic colorDayBlue;
+@dynamic reflectionText;
+@dynamic timeStamp;
+
+@end
