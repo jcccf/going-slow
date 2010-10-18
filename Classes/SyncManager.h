d4609c8fcb088361868ec9622076c91832539af0
gmthomas25 <gmthomas25@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-10-18 21:46:41 +0000

    Added_Some skeleton interface

---
+++ Classes/SyncManager.h
@@ -0,0 +1,16 @@
+//
+//  SyncManager.h
+//  goslowtest2
+//
+//  Created by Gregory Thomas on 10/18/10.
+//  Copyright 2010 Cornell University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SyncManager : NSObject {
+
+}
+
+@end
