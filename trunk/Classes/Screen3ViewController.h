6730e6a7b64439b13ccc30a18098ca408c2a8f9f
gmthomas25 <gmthomas25@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-10-25 03:19:47 +0000

    Reflection Screen Update

--- trunk/Classes/Screen3ViewController.h
+++ trunk/Classes/Screen3ViewController.h
@@ -8,11 +8,46 @@
 
 #import <UIKit/UIKit.h>
 #import "Reflection.h"
+#import "ReflectColorView.h"
+#import "ReflectCameraView.h"
+#import "ReflectTextView.h"
 
 @interface Screen3ViewController : UIViewController {
 
+	IBOutlet UIView *reflectView;
+	IBOutlet UIButton *textButton;
+	IBOutlet UIButton *cameraButton;
+	IBOutlet UIButton *colorButton;
+	IBOutlet UIBarItem *saveButton;
+	IBOutlet UIBarItem *backButton;
+	IBOutlet ReflectColorView *colorView;
+	IBOutlet ReflectCameraView *cameraView;
+	IBOutlet UITextView *textView;
+	
 }
 
 -(void)storeReflection:(Reflection *)r;
+
+@property(nonatomic,retain) UIView *reflectView;
+@property(nonatomic,retain) UIButton *textButton;
+@property(nonatomic,retain) UIButton *cameraButton;
+@property(nonatomic,retain) UIButton *colorButton;
+@property(nonatomic,retain) UIBarItem *saveButton;
+@property(nonatomic,retain) UIBarItem *backButton;
+@property(nonatomic,retain) ReflectColorView *colorView;
+@property(nonatomic,retain) ReflectCameraView *cameraView;
+@property(nonatomic,retain) UITextView *textView;
+
+-(IBAction)goToText:(id)sender;
+
+-(IBAction)changeColor:(id)sender;
+	
+
+-(IBAction)goToCamera:(id)sender;
+	
+
+-(IBAction)goToColor:(id)sender;
+	
+-(IBAction)goBack:(id)sender;
 
 @end
