285b64b7a25ccf2ff22a5c3bf98d6058e8ad4f02
gmthomas25 <gmthomas25@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-10-28 01:29:26 +0000

    History_Screen_skeleton added

---
+++ trunk/Classes/HistoryTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  HistoryTableViewController.h
+//  goslowtest2
+//
+//  Created by Gregory Thomas on 10/27/10.
+//  Copyright 2010 Cornell University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HistoryReflectionViewController.h"
+
+@interface HistoryTableViewController : UITableViewController {
+	HistoryReflectionViewController *histRefViewCont;
+}
+
+@property(nonatomic,retain) HistoryReflectionViewController *histRefViewCont;
+@end
