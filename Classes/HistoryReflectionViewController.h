0dae1205f4659b5057c852cd094d9e7c76ba70a1
gmthomas25 <gmthomas25@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-10-28 01:29:26 +0000

    History_Screen_skeleton added

---
+++ Classes/HistoryReflectionViewController.h
@@ -0,0 +1,17 @@
+//
+//  HistoryReflectionViewController.h
+//  goslowtest2
+//
+//  Created by Gregory Thomas on 10/27/10.
+//  Copyright 2010 Cornell University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HistoryReflectionViewController : UIViewController {
+	//UINavigationItem *navigationItem;
+}
+
+//@property(nonatomic,retain) IBOutlet UINavigationItem *navigationItem;
+@end
