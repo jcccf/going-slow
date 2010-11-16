c80146cfedb7a403722eb8f3ff1a72ed88ec3ec1
akshaybapat <akshaybapat@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-11-16 02:39:59 +0000

    New Setup Screen

--- Classes/NotificationTime.m
+++ Classes/NotificationTime.m
@@ -10,27 +10,30 @@
 
 
 @implementation NotificationTime
-@synthesize datePicker, eventText, delegateReference, morningEvening, morningDate, eveningDate;
+@synthesize datePicker, delegateReference, doneButton, dataArray, dateFormatter, infoButton, dataArray2;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*-(id) init{
-	self = [super initWithNibName:@"NotificationTime" bundle:nil];
-	
-	return self;
-	
-}*/
 
 
 -(IBAction)goToHomeScreen:(id)sender{
+	CGRect screenRect = [[UIScreen mainScreen] applicationFrame];
+	CGRect endFrame = self.datePicker.frame;
+	endFrame.origin.y = screenRect.origin.y + screenRect.size.height;
+	
+	// start the slide down animation
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration:0.3];
+	
+	// we need to perform some post operations after the animation is complete
+	[UIView setAnimationDelegate:self];
+	[UIView setAnimationDidStopSelector:@selector(slideDownDidStop)];
+	
+	self.datePicker.frame = endFrame;
+	[UIView commitAnimations];
+	
+	// grow the table back again in vertical size to make room for the date picker
+	CGRect newFrame = self.tableView.frame;
+	newFrame.size.height += self.datePicker.frame.size.height;
+	self.tableView.frame = newFrame;
 //	//[self.view removeFromSuperview];
 //	[[UIApplication sharedApplication] cancelAllLocalNotifications];
 //	
@@ -120,57 +123,135 @@
 //	// Schedule the notification
 //    [[UIApplication sharedApplication] scheduleLocalNotification:localNotif];
 //    [localNotif release];*/
+	NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
 	
-	[[SuggestionList getInstance] scheduleNotifications];
-	
-	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Your notifications times have been updated" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
-	[alert show];
-	[alert release];
-	//[delegateReference.window addSubview:delegateReference.tabController.view];
-	
-}
-
--(IBAction)dateChanged:(id)sender{
-	if(morningEvening.selectedSegmentIndex == 0){
+	if([indexPath row] == 0) {
 		morningDate = [datePicker date];
 		[morningDate retain];
 		NSLog(@"Changing morning date");
+		NSLog([morningDate description]);
 	}
-	else{
-		NSLog(@"Changing evening date");
+	if([indexPath row] == 1) {
 		eveningDate = [datePicker date];
 		[eveningDate retain];
+		NSLog(@"Changing evening date");
+		NSLog([eveningDate description]);
 	}
+	
+		// remove the "Done" button in the nav bar
+	
+	self.navigationItem.rightBarButtonItem = self.infoButton;
+	
+	[self.tableView deselectRowAtIndexPath:indexPath animated:YES];
 	
 	[[SuggestionList getInstance] setDate:morningDate eveningDate: eveningDate];
+
+	[[SuggestionList getInstance] scheduleNotifications];
 	
 }
 
--(IBAction)segmentChange:(id)sender{
-	if(morningEvening.selectedSegmentIndex == 0){
-		NSLog(@"InsideMorning Segment");
-		[datePicker setDate:morningDate];
-	}
-	else {
-		NSLog(@"Inside Evening Segment");
-		NSLog([eveningDate description]);
-		assert(eveningDate != nil);
-		[datePicker setDate:eveningDate];
-	}
+	
 
+- (IBAction)dateAction:(id)sender
+{
+	NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+	UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+	cell.detailTextLabel.text = [self.dateFormatter stringFromDate:self.datePicker.date];
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return [self.dataArray count];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	UITableViewCell *targetCell = [tableView cellForRowAtIndexPath:indexPath];
+	self.datePicker.date = [self.dateFormatter dateFromString:targetCell.detailTextLabel.text];
+	
+	// check if our date picker is already on screen
+	if (self.datePicker.superview == nil)
+	{
+		[self.view.window addSubview: self.datePicker];
+		
+		// size up the picker view to our screen and compute the start/end frame origin for our slide up animation
+		//
+		// compute the start frame
+		CGRect screenRect = [[UIScreen mainScreen] applicationFrame];
+		CGSize pickerSize = [self.datePicker sizeThatFits:CGSizeZero];
+		CGRect startRect = CGRectMake(0.0,
+									  screenRect.origin.y + screenRect.size.height,
+									  pickerSize.width, pickerSize.height);
+		self.datePicker.frame = startRect;
+		
+		// compute the end frame
+		CGRect pickerRect = CGRectMake(0.0,
+									   screenRect.origin.y + screenRect.size.height - pickerSize.height,
+									   pickerSize.width,
+									   pickerSize.height);
+		// start the slide up animation
+		[UIView beginAnimations:nil context:NULL];
+		[UIView setAnimationDuration:0.3];
+		
+		// we need to perform some post operations after the animation is complete
+		[UIView setAnimationDelegate:self];
+		
+		self.datePicker.frame = pickerRect;
+		
+		[UIView commitAnimations];
+		
+		// add the "Done" button to the nav bar
+		self.navigationItem.rightBarButtonItem = self.doneButton;
+	}
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	static NSString *kCustomCellID = @"CustomCellID";
+	
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCustomCellID];
+	if (cell == nil)
+	{
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:kCustomCellID] autorelease];
+	}
+	
+	cell.textLabel.text = [self.dataArray objectAtIndex:indexPath.row];
+	cell.detailTextLabel.text = [self.dataArray2 objectAtIndex:indexPath.row];
+	
+	return cell;
+}
+
+- (void)slideDownDidStop
+{
+	// the date picker has finished sliding downwards, so remove it
+	[self.datePicker removeFromSuperview];
+}
+
+-(IBAction)showInfo:(id)sender{
+	
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"About" message:@"Go Slow is an application that helps you reduce stress. It gives you a suggestion in the morning and a reminder to reflect in the evening."
+												   delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+	[alert show];
+	[alert release];
+	
+}
+
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+	self.navigationItem.rightBarButtonItem = self.infoButton;
 	//delegateReference = [[UIApplication sharedApplication] delegate];
+	self.dataArray = [NSArray arrayWithObjects:@"Morning Reminder", @"Evening Reminder", nil];
+	
+	self.dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
+	[self.dateFormatter setDateStyle:NSDateFormatterNoStyle];
+	[self.dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+	//self.infoButton
 	
 	NSCalendar *calendar = [NSCalendar autoupdatingCurrentCalendar];
 	
-	//[[UIApplication sharedApplication] cancelAllLocalNotifications];
-	
     // Get the current date
     NSDate *pickerDate = [NSDate date];
-	
     // Break the date up into components
     NSDateComponents *dateComponents = [calendar components:( NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit )
 												   fromDate:pickerDate];
@@ -186,96 +267,26 @@
 	[dateComps setMinute:0];
 	[dateComps setSecond:0];
 	
-	//[dateComp1 setHour:22];
-	//[dateComp1 setMinute:0];
-
-	// Notification will fire in one minute
-	//[dateComps setSecond:[timeComponents second]];
+	
 	morningDate =[calendar dateFromComponents:dateComps];
 	[dateComps setHour:22];
 	eveningDate = [calendar dateFromComponents:dateComps];
 	[eveningDate retain];
 	[morningDate retain];
-	NSLog([eveningDate description]);
+	
+	self.dataArray2=[NSArray arrayWithObjects:[self.dateFormatter stringFromDate:morningDate], [self.dateFormatter stringFromDate:eveningDate], nil];
 	
 	[[SuggestionList getInstance] setDate:morningDate eveningDate: eveningDate];
 	
+	[[SuggestionList getInstance] scheduleNotifications];
+	
+	NSLog([eveningDate description]);
 	[datePicker setDate:morningDate];
 	[dateComps release];
 	
     [super viewDidLoad];
-	
-	//[[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
 }
 
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-
-
-- (IBAction) removeAlarm:(id) sender{
-	
-}
-- (IBAction) scheduleAlarm:(id) sender {
-	
-	//cancels all other notifications assigned to this application
-	
-	[[UIApplication sharedApplication] cancelAllLocalNotifications];
-	
-	NSCalendar *calendar = [NSCalendar autoupdatingCurrentCalendar];
-	
-    // Get the current date
-    NSDate *pickerDate = [datePicker date];
-	
-    // Break the date up into components
-    NSDateComponents *dateComponents = [calendar components:( NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit )
-												   fromDate:pickerDate];
-    NSDateComponents *timeComponents = [calendar components:( NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit )
-												   fromDate:pickerDate];
-    // Set up the fire time
-    NSDateComponents *dateComps = [[NSDateComponents alloc] init];
-    [dateComps setDay:[dateComponents day]];
-    [dateComps setMonth:[dateComponents month]];
-    [dateComps setYear:[dateComponents year]];
-    [dateComps setHour:[timeComponents hour]];
-	// Notification will fire in one minute
-    [dateComps setMinute:[timeComponents minute]];
-	[dateComps setSecond:[timeComponents second]];
-    NSDate *itemDate = [calendar dateFromComponents:dateComps];
-    [dateComps release];
-	
-    UILocalNotification *localNotif = [[UILocalNotification alloc] init];
-    if (localNotif == nil)
-        return;
-	localNotif.fireDate = [itemDate dateByAddingTimeInterval:1];
-	
-	//1 day repeat interval
-	localNotif.repeatInterval = NSDayCalendarUnit;
-	// localNotif.fireDate = [NSDate dateWithTimeIntervalSinceNow:20];
-    localNotif.timeZone = [NSTimeZone defaultTimeZone];
-	
-	// Notification details
-    localNotif.alertBody = @"You should reflect";
-	// Set the action button
-    localNotif.alertAction = @"Reflect";
-	
-    localNotif.soundName = UILocalNotificationDefaultSoundName;
-    localNotif.applicationIconBadgeNumber = 1;
-	
-	// Specify custom data for the notification
-    NSDictionary *infoDict = [NSDictionary dictionaryWithObject:@"someValue" forKey:@"someKey"];
-    localNotif.userInfo = infoDict;
-	
-	// Schedule the notification
-    [[UIApplication sharedApplication] scheduleLocalNotification:localNotif];
-    [localNotif release];
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -284,7 +295,6 @@
 - (void)viewDidUnload {
 	datePicker = nil;
 	//tableview = nil;
-	eventText = nil;
 }
 
 - (void)dealloc {
