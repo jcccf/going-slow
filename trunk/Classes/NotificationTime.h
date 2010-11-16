f56188173bb03c39d86ffcd3b60d1dc9571f99ce
akshaybapat <akshaybapat@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-11-16 02:39:59 +0000

    New Setup Screen

--- trunk/Classes/NotificationTime.h
+++ trunk/Classes/NotificationTime.h
@@ -10,30 +10,34 @@
 #import "goslowtest2AppDelegate.h"
 #import "SuggestionList.h"
 
-@interface NotificationTime : UIViewController {
+@interface NotificationTime : UITableViewController {
 	//IBOutlet UITableView *tableview;
 	IBOutlet UIDatePicker *datePicker;
-	IBOutlet UITextField *eventText;
 	goslowtest2AppDelegate *delegateReference;
-	IBOutlet UISegmentedControl *morningEvening;
 	NSDate *morningDate;
 	NSDate *eveningDate;
+	UIBarButtonItem *doneButton;    // this button appears only when the date picker is open	
+	NSArray *dataArray;	
+	NSArray *dataArray2;	
+	NSDateFormatter *dateFormatter;
+	UIBarButtonItem *infoButton;
 }
 
 //@property (nonatomic, retain) IBOutlet UITableView *tableview;
 @property (nonatomic, retain) IBOutlet UIDatePicker *datePicker;
-@property (nonatomic, retain) IBOutlet UITextField *eventText;
 @property (nonatomic, retain) goslowtest2AppDelegate *delegateReference;
-@property (nonatomic, retain) IBOutlet UISegmentedControl *morningEvening;
 @property (nonatomic,assign) NSDate *morningDate;
 @property (nonatomic,assign) NSDate *eveningDate;
+@property (nonatomic, retain) NSArray *dataArray; 
+@property (nonatomic, retain) NSArray *dataArray2; 
+@property (nonatomic, retain) NSDateFormatter *dateFormatter; 
+@property (nonatomic, retain) IBOutlet UIBarButtonItem *doneButton;
+@property (nonatomic, retain) IBOutlet UIBarButtonItem *infoButton;
 
 
-- (IBAction) scheduleAlarm:(id) sender;
-- (IBAction) removeAlarm:(id) sender;
 - (IBAction) goToHomeScreen:(id)sender;
--(IBAction) dateChanged:(id)sender;
--(IBAction) segmentChange:(id)sender;
+-(IBAction) dateAction:(id)sender;
+-(IBAction) showInfo:(id)sender;
 
 @end
 
