0cce6ac1b16e388b28237ecb64f2de1f4118d3e9
gmthomas25 <gmthomas25@70977530-8bf9-22b5-0da3-487dd241dcef>
2010-11-23 09:41:13 +0000

    scroll screen framework added

---
+++ Classes/ScrollDiaryScreenController.h
@@ -0,0 +1,30 @@
+//
+//  ScrollDiaryScreenController.h
+//  goslowtest2
+//
+//  Created by Gregory Thomas on 11/23/10.
+//  Copyright 2010 Cornell University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PageManager;
+
+
+@interface ScrollDiaryScreenController : UIViewController <UIScrollViewDelegate>{
+
+	IBOutlet UIScrollView *scrollView;
+	IBOutlet UITableView *dateTableView;
+	IBOutlet UIPageControl *pageControl;
+	
+	NSMutableArray *viewControllers;
+}
+
+@property(nonatomic,retain) UIScrollView *scrollView;
+@property(nonatomic,retain) UITableView *dateTableView;
+@property(nonatomic,retain) UIPageControl *pageControl;
+@property(nonatomic,retain) NSMutableArray *viewControllers;
+
+
+-(void)loadScrollViewWithPage:(int)page;
+@end
